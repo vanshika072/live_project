5d23021c243fb9bb4da82eb1ef3d6c881bebb8c0
vanshika072 <139579928+vanshika072@users.noreply.github.com>
2023-10-03 00:30:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from tkinter import *
+bot = ChatBot("My Bot")
+
+convo = [
+    'Hello',
+    'Hi there !',
+    'What is your name ?',
+    'My name is Bot , I am created by Vanshika Gupta',
+    'How are you ?',
+    'I am doing great these days',
+    'In which country you live ?',
+    'I live in India',
+    'In which language you talk?',
+    'I mostly talk in english'
+]
+
+trainer = ListTrainer(bot)
+
+# now training the bot with the help of trainer
+
+trainer.train(convo)
+
+# answer = bot.get_response("what is your name?")
+# print(answer)
+
+# print("Talk to bot ")
+# while True:
+#     query = input()
+#     if query == 'exit':
+#         break
+#     answer = bot.get_response(query)
+#     print("bot : ", answer)
+
+main = Tk()
+
+main.geometry("500x650")
+
+main.title("My Chat bot")
+img = PhotoImage(file="bot1.png")
+
+photoL = Label(main, image=img)
+
+photoL.pack(pady=5)
+
+
+
+def ask_from_bot():
+    query = textF.get()
+    answer_from_bot = bot.get_response(query)
+    msgs.insert(END, "you : " + query)
+    print(type(answer_from_bot))
+    msgs.insert(END, "bot : " + str(answer_from_bot))
+    # speak(answer_from_bot)
+    textF.delete(0, END)
+    # msgs.yview(END)
+
+
+frame = Frame(main)
+
+sc = Scrollbar(frame)
+msgs = Listbox(frame, width=80, height=20)
+
+sc.pack(side=RIGHT, fill=Y)
+
+msgs.pack(side=LEFT, fill=BOTH, pady=10)
+
+frame.pack()
+
+# creating text field
+
+textF = Entry(main, font=("Verdana", 20))
+textF.pack(fill=X, pady=10)
+
+btn = Button(main, text="Ask from bot", font=("Verdana", 20), command=ask_from_bot)
+btn.pack()
+main.mainloop()
